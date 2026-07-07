8d03da3773eb09fa6a8035abb798b4250671870c
eoFrancaa <erfranca.com@gmail.com>
2026-07-07 10:05:51 -0300

    url images

--- src/data/products.js
+++ src/data/products.js
@@ -4,41 +4,41 @@ export const products = [
     name: "Big Mac",
     category: "Hambúrgueres",
     price: 29.90,
-    image: "https://placehold.co/300x220?text=Big+Mac"
+    image: "https://i.postimg.cc/gXsBSrcq/Big-Mc.jpg"
   },
   {
     id: 2,
     name: "Quarterão",
     category: "Hambúrgueres",
     price: 32.90,
-    image: "https://placehold.co/300x220?text=Quarterao"
+    image: "https://i.postimg.cc/7bPRC1mZ/Mc-Quarter.jpg"
   },
   {
     id: 3,
     name: "McChicken",
     category: "Hambúrgueres",
     price: 27.90,
-    image: "https://placehold.co/300x220?text=McChicken"
+    image: "https://i.postimg.cc/SjQPXLDK/Mc-Chicken.jpg"
   },
   {
     id: 4,
     name: "Batata Média",
     category: "Acompanhamentos",
     price: 11.90,
-    image: "https://placehold.co/300x220?text=Batata"
+    image: "https://i.postimg.cc/bdY5G03N/Mc-Fries.jpg"
   },
   {
     id: 5,
     name: "Coca-Cola",
     category: "Bebidas",
     price: 8.90,
-    image: "https://placehold.co/300x220?text=Coca-Cola"
+    image: "https://i.postimg.cc/MM5rPXZD/Coca-Cola.jpg"
   },
   {
     id: 6,
     name: "Sundae",
     category: "Sobremesas",
     price: 9.90,
-    image: "https://placehold.co/300x220?text=Sundae"
+    image: "https://i.postimg.cc/qgBFtcjR/Sundae.jpg"
   }
 ]
